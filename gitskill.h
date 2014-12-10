28570106b20bd81430c5a1ec4b7a3de550624b57
root <root@localhost.localdomain>
2014-12-10 10:17:30 -0600

    add new file gitskill.h

---
+++ gitskill.h
@@ -0,0 +1,10 @@
+/*************************************************************************
+    > File Name: gitskill.h
+    > Author: wikkas
+    > Mail: wikkas@163.com 
+    > Created Time: Wed Dec 10 10:13:25 2014
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
