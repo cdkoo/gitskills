dc3b7dd1732d36f0fb1e3729ac37086f77c14c13
root <root@localhost.localdomain>
2014-12-10 09:41:49 -0600

    add new file gitskill.cpp

---
+++ gitskill.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+    > File Name: gitskill.cpp
+    > Author: wikkas
+    > Mail: wikkas@163.com 
+    > Created Time: Wed Dec 10 09:41:16 2014
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
